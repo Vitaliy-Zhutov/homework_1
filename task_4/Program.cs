840dace4b991071f395038cbb59037ea2a42a376
vitaly <vitaly.zutov@yandex.ru>
2022-12-18 23:16:25 +0700

    Решение Задачи 4, найти максимальное из 3х чисел

---
+++ task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+using static System.Console;
+
+Write ("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+Write ("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+Write ("Введите третье число: ");
+int c = int.Parse(ReadLine()!);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Write("Максимальное число: ");
+WriteLine(max);
