344edb962932cb6d7480fedb16d39a0b221176aa
vitaly <vitaly.zutov@yandex.ru>
2022-12-18 21:16:41 +0700

    Решение задачи на наибольшее и наименьшее значение из двух чисел

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+using static System.Console;
+
+Write ("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+Write ("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+if (a > b)
+
+{
+    WriteLine($"Большее число = {a}, менешее число = {b}");
+}
+
+else
+
+{
+    WriteLine($"Большее число = {b} менешее число = {a}");
+}
